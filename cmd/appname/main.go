fdccc9a28efd690bc751fc5bba98907c0b92efcc
李梦杰 <17638429024@163.com>
2025-08-05 16:49:30 +0800

    初始化结构，添加文件夹

---
+++ cmd/appname/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"NewProject/internal/pkg1"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(pkg1.Demo1())
+}
---
+++ internal/pkg1/frist_pkg.go
@@ -0,0 +1,6 @@
+package pkg1
+
+func Demo1() string {
+	//fmt.Println("内部包，只写本项目可访问的文件")
+	return "内部包，只写本项目可访问的文件"
+}
