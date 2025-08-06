6562ca2f1adcc937b3593e85cd9289398e6c6bcd
李梦杰 <17638429024@163.com>
2025-08-06 18:58:52 +0800

    联系有符号整数和无符号整数

--- cmd/appname/main.go
+++ cmd/appname/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
-	"NewProject/internal/pkg1"
 	"fmt"
 )
 
 func main() {
 	//fmt.Println("测试")
-	fmt.Println(pkg1.Demo1())
+	//fmt.Println(pkg1.Demo1())
 	//var name = "limengjie"
 	//fmt.Println(name)
-
+	//var _name = "limengjies"
+	//fmt.Println(_name)
+	//const (
+	//	A = iota
+	//	B = iota
+	//)
+	//fmt.Printf("A: %s\n", A)
+	//fmt.Println("B", B)
+	var X int8 = 127    // int8有符号整数的取值范围是 -128~127
+	fmt.Println(X+4, X) // 对超出的范围，go语言设计了补码饶回，即：127 + 1 = -128 + 1 = -127 + 1 = -126 + 1 = -125，最后输出-125
+	var Y int16 = 32767 // int16有符号整数的取值范围是：-32768~32767
+	fmt.Println(Y+2, Y) // 同上，最后输出是 -32767
+	var Z uint8 = 255   // uint无符号整数的取值范围是0~255，对超出的部分也会进行补码绕回
+	fmt.Println(Z+1, Z) //最后输出   0
 }
--- internal/pkg1/frist_pkg.go
+++ internal/pkg1/frist_pkg.go
@@ -1,6 +1,7 @@
 package pkg1
 
 func Demo1() string {
-	return "内部包，只写本项目可访问的文件"
+	var _a = "外部包，打印外部变量"
+	return "内部包，只写本项目可访问的文件\n" + _a
 	//return 1 + 1
 }
