9e54c0efcd3e3e19e215fde93b6dbb1a09151469
李梦杰 <17638429024@163.com>
2025-08-07 08:55:04 +0800

    go中的基本类型操作包括，短变量声明（声明变量+初始化，让程序本身自己理解当前数据类型）

--- cmd/appname/main.go
+++ cmd/appname/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	//fmt.Println("测试")
@@ -17,10 +15,24 @@ func main() {
 	//)
 	//fmt.Printf("A: %s\n", A)
 	//fmt.Println("B", B)
-	var X int8 = 127    // int8有符号整数的取值范围是 -128~127
-	fmt.Println(X+4, X) // 对超出的范围，go语言设计了补码饶回，即：127 + 1 = -128 + 1 = -127 + 1 = -126 + 1 = -125，最后输出-125
-	var Y int16 = 32767 // int16有符号整数的取值范围是：-32768~32767
-	fmt.Println(Y+2, Y) // 同上，最后输出是 -32767
-	var Z uint8 = 255   // uint无符号整数的取值范围是0~255，对超出的部分也会进行补码绕回
-	fmt.Println(Z+1, Z) //最后输出   0
+	//var X int8 = 127    // int8有符号整数的取值范围是 -128~127
+	//fmt.Println(X+4, X) // 对超出的范围，go语言设计了补码饶回，即：127 + 1 = -128 + 1 = -127 + 1 = -126 + 1 = -125，最后输出-125
+	//var Y int16 = 32767 // int16有符号整数的取值范围是：-32768~32767
+	//fmt.Println(Y+2, Y) // 同上，最后输出是 -32767
+	//var Z uint8 = 255   // uint无符号整数的取值范围是0~255，对超出的部分也会进行补码绕回
+	//fmt.Println(Z+1, Z) //最后输出   0
+	//a := 20.45
+	//b := 100.32
+	//fmt.Println(a + b)
+	//z := 3.0 + 4.0i
+	//module := math.Hypot(real(z), imag(z))
+	//fmt.Println("module: ", module)
+	//fmt.Println(real(z), imag(z))
+	a := 123
+	//b := "123"
+	//c := "456"
+	//result := a == (b + c)
+	//fmt.Println(result)
+	//fmt.Println(len(a))
+	fmt.Printf("a的类型是：%d", a)
 }
